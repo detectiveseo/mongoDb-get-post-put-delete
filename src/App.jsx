07ff5705829a019ecb7a485f7fc789b240c980f1
detectiveseo <detectiveseo1@gmail.com>
2023-07-08 19:14:16 +0600

    edit popup for items

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,95 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import addToDb from './functions/addToDb'
 import axios from 'axios'
+import EditItem from './Components/EditItem'
 
 function App() {
+
+  const [editForm, setEditForm] = useState(true);
+  const [editFormData, setEditFormData] = useState({})
+
+
   let [allData, setAllData] = useState([]);
-  useState(() => {
+  useEffect(() => {
     axios.get("http://localhost:3000/users").then(data => setAllData(data.data))
   }, [addToDb])
 
 
-  const deleteItems = (x) =>{
+  const deleteItems = (x) => {
     const id = x._id;
     axios.delete(`http://localhost:3000/remove/${id}`)
-    .then(res => console.log(res))
+      .then(() => { })
+  }
+
+
+  const editItems = (x) => {
+    setEditFormData(x);
+    const id = x._id;
+    // <TODO></TODO>
+    setEditForm(false);
+    return x;
   }
   return (
     <>
-      <div className='p-10 border rounded-lg shadow-2xl shadow-lime-400 text-white'>
-        <h2 className='text-3xl uppercase mb-10'>Send data to mongodb</h2>
-        <form
-          onSubmit={addToDb}
-          className='flex flex-col gap-4'>
-          <input
-            type="text"
-            placeholder="Type here"
-            name='name'
-            value="Imran"
-            className="input input-bordered input-success w-full" />
+      <div className='grid grid-cols-2 gap-4 relative'>
 
-          <input
-            type="text"
-            placeholder="Type here"
-            name='email'
-            value="imboy8585@gmail.com"
-            className="input input-bordered input-success w-full" />
+        <div className='p-10 border rounded-lg sticky h-[95vh] top-0 shadow-2xl shadow-lime-400 text-white'>
+          <h2 className='text-3xl uppercase mb-10'>Send data to mongodb</h2>
+          <form
+            onSubmit={addToDb}
+            className='flex flex-col gap-4'>
+            <input
+              type="text"
+              placeholder="Type here"
+              name='name'
+              value="Imran"
+              className="input input-bordered input-success w-full" />
 
-          <input
-            type="text"
-            placeholder="Type here"
-            name='proffesion'
-            value="freeluncer"
-            className="input input-bordered input-success w-full" />
-          <input type="submit" value="submit" className='btn btn-outline w-full bg-green-300 text-black shadow-green-400 shadow-inner' />
-        </form>
+            <input
+              type="text"
+              placeholder="Type here"
+              name='email'
+              value="imboy8585@gmail.com"
+              className="input input-bordered input-success w-full" />
+
+            <input
+              type="text"
+              placeholder="Type here"
+              name='proffesion'
+              value="freeluncer"
+              className="input input-bordered input-success w-full" />
+            <input type="submit" value="submit" className='btn btn-outline w-full bg-green-300 text-black shadow-green-400 shadow-inner' />
+          </form>
+        </div>
+
+        <div className='grid grid-cols-2 gap-5 justify-start items-center'>
+          {
+            allData.map((x, i) => {
+              return (
+                <div key={i} className='border flex-col h-56 flex justify-center items-center relative'>
+                  <h2 className='text-3xl text-white'>{x?.name}</h2>
+                  <h2 className='text-1xl text-white'>{x?.email}</h2>
+
+                  <div
+                    onClick={() => deleteItems(x)}
+                    className='btn btn-outline bg-red-700 p-3 absolute -top-5 -right-5 rounded-full text-white px-5'>X</div>
+                  <div
+                    onClick={() => editItems(x)}
+                    className='bg-red-600 absolute bottom-0 left-0 p-3 text-white px-5'>Edit</div>
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
 
-      <div className='grid grid-cols-3 gap-5 justify-start items-center'>
-        {
-          allData.map((x, i) => {
-            return (
-              <div key={i} className='border flex-col h-48 flex justify-center items-center relative'>
-                <h2 className='text-3xl text-white'>{x?.name}</h2>
-                <h2 className='text-1xl text-white'>{x?.email}</h2>
-                <div 
-                onClick={() => deleteItems(x)}
-                className='bg-red-600 absolute bottom-2 p-3 rounded-full text-white px-5'>X</div>
-              </div>
-            )
-          })
-        }
+      <div className={` fixed w-full left-0 h-[100vh] top-0 bg-gray-700 ${editForm ? "hidden" : "block"} `}>
+        <div className='w-4/12 mx-auto flex flex-col justify-center h-[100vh]'>
+          <EditItem editFormData={editFormData} setEditForm={setEditForm}/>
+        </div>
       </div>
     </>
   )
---
+++ src/Components/EditItem.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const EditItem = ({editFormData ,setEditForm}) => {
+
+    const {name, email, proffesion} = editFormData;
+    return (
+        <div className='p-10 border bg-gray-700 rounded-lg sticky top-0 shadow-2xl shadow-lime-400 text-white'>
+            <h2 className='text-3xl uppercase mb-10'>Send data to mongodb</h2>
+            <form
+                className='flex flex-col gap-4'>
+                <input
+                    type="text"
+                    placeholder="Type here"
+                    name='name'
+                    value={name}
+                    className="input input-bordered input-success w-full" />
+
+                <input
+                    type="text"
+                    placeholder="Type here"
+                    name='email'
+                    value={email}
+                    className="input input-bordered input-success w-full" />
+
+                <input
+                    type="text"
+                    placeholder="Type here"
+                    name='proffesion'
+                    value={proffesion}
+                    className="input input-bordered input-success w-full" />
+                <input type="submit" value="submit" className='btn btn-outline w-full bg-green-300 text-black shadow-green-400 shadow-inner' />
+            </form>
+            <div 
+            onClick={() =>  setEditForm(true)}
+            className=' absolute btn btn-outline -top-4 -right-4 bg-slate-400 text-white'>X</div>
+        </div>
+    );
+};
+
+export default EditItem;
